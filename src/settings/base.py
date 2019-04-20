2ff5cd4c7249a5edbcf30bd0aa45f2c798ab7d27
Lincoln Karuhanga <karuhanga475@gmail.com>
2019-04-20 17:47:55 +0300

    - Reduce page size

--- src/settings/base.py
+++ src/settings/base.py
@@ -124,5 +124,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, '../media')
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 5
+    'PAGE_SIZE': 1
 }
